02ed67cecd2b0043b4e3c08b64d87b5ee19fdeec
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-18 08:35:42 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,12 @@ class SessionsController < ApplicationController
 
   def create
     session[:name] = params[:name]
-    redirect_to '/'
+    
   end
 
   def destroy
     session.delete :name
+    redirect_to '/'
   end
 
 end

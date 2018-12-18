b1cf13c11174f72339042d6ae5d9c3ad556ae529
Bryce Eadie <bryce.eadie@gmail.com>
2018-12-18 08:40:46 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     if params[:name].present?
       session[:name] = params[:name]
-      redirect_to "/home"
+      redirect_to "/"
     else
       redirect_to "/login"
     end
